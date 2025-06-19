a735b7b32a7ec3a76b8bc59dab928db2c92985a0
farrasnazhif <farrasnazhif@gmail.com>
2025-06-19 12:23:17 +0700

    Feat: Project Setup

---
+++ lib/constants/index.ts
@@ -0,0 +1,5 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "SEA Catering";
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION || "Healthy Meals, Anytime, Anywhere";
+export const SERVER_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
