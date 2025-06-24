1249087b26e62a00cd1e352887f6099d5a1a1c88
farrasnazhif <farrasnazhif@gmail.com>
2025-06-24 08:22:54 +0700

    Feat: Setup Prisma & Auth

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -3,3 +3,19 @@ export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION || "Healthy Meals, Anytime, Anywhere";
 export const SERVER_URL =
   process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
+
+export const signInDefaultValues = {
+  email: "",
+  password: "",
+};
+
+export const signUpDefaultValues = {
+  name: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
+
+export const USER_ROLES = process.env.USER_ROLES
+  ? process.env.USER_ROLES.split(",")
+  : ["admin", "user"];
