37b7dca38f1470f2116b22f695b310c294ff5fb9
farrasnazhif <farrasnazhif@gmail.com>
2025-06-25 20:36:17 +0700

    Feat: Checkout Steps UI

---
+++ app/(root)/about-you/page.tsx
@@ -0,0 +1,11 @@
+import CheckoutSteps from "@/components/shared/checkout-steps";
+
+const AboutYouPage = () => {
+  return (
+    <>
+      <CheckoutSteps current={0} />
+    </>
+  );
+};
+
+export default AboutYouPage;
--- app/(root)/subscription/page.tsx
+++ app/(root)/subscription/page.tsx
@@ -1,8 +1,10 @@
+import CheckoutSteps from "@/components/shared/checkout-steps";
 import SubscriptionForm from "./subscription-form";
 
 const SubscriptionPage = () => {
   return (
     <>
+      <CheckoutSteps current={1} />
       <SubscriptionForm />
     </>
   );
