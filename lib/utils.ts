cd7fad5397de22107b151ca45df5a2fe4eb60fc1
farrasnazhif <farrasnazhif@gmail.com>
2025-06-30 20:32:52 +0700

    Feat: Date Time Format

--- lib/utils.ts
+++ lib/utils.ts
@@ -57,3 +57,46 @@ export function formatError(error: any): FormErrorObject {
 export function getErrorMessage(msg: string | { message: string }): string {
   return typeof msg === "string" ? msg : msg.message;
 }
+
+// Format date and times (indonesia)
+export const formatDateTime = (dateString: Date) => {
+  const dateTimeOptions: Intl.DateTimeFormatOptions = {
+    month: "short", // abbreviated month name (e.g., 'Oct')
+    year: "numeric", // abbreviated month name (e.g., 'Oct')
+    day: "numeric", // numeric day of the month (e.g., '25')
+    hour: "numeric", // numeric hour (e.g., '8')
+    minute: "numeric", // numeric minute (e.g., '30')
+    hour12: false, // use 12-hour clock (true) or 24-hour clock (false)
+    timeZone: "Asia/Jakarta", // waktu Indonesia (WIB)
+  };
+  const dateOptions: Intl.DateTimeFormatOptions = {
+    weekday: "short", // abbreviated weekday name (e.g., 'Mon')
+    month: "short", // abbreviated month name (e.g., 'Oct')
+    year: "numeric", // numeric year (e.g., '2023')
+    day: "numeric", // numeric day of the month (e.g., '25')
+    timeZone: "Asia/Jakarta", // waktu Indonesia (WIB)
+  };
+  const timeOptions: Intl.DateTimeFormatOptions = {
+    hour: "numeric", // numeric hour (e.g., '8')
+    minute: "numeric", // numeric minute (e.g., '30')
+    hour12: false, // use 12-hour clock (true) or 24-hour clock (false)
+    timeZone: "Asia/Jakarta", // waktu Indonesia (WIB)
+  };
+  const formattedDateTime: string = new Date(dateString).toLocaleString(
+    "en-ID",
+    dateTimeOptions
+  );
+  const formattedDate: string = new Date(dateString).toLocaleString(
+    "en-ID",
+    dateOptions
+  );
+  const formattedTime: string = new Date(dateString).toLocaleString(
+    "en-ID",
+    timeOptions
+  );
+  return {
+    dateTime: formattedDateTime,
+    dateOnly: formattedDate,
+    timeOnly: formattedTime,
+  };
+};
